e2c656b2d1ba656be39ea49f567023e530f77ee5
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 18:59:04 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"My car battery has gone dead. I will have it ............... tomorrow.",
+        options:["replaced", "readjusted", "reformed", "rearranged", "reproduced"],
+        answer: 0
+    },
+    {
+        q:"We need two ............... men to volunteer for this dangerous operation.",
+        options:["panicky", "brave", "involuntary", "timid", "clumsy"],
+        answer: 1
+    },
+    {
+        q:"Due to a nationwide nurses' ............... , all clinics will be closed today.",
+        options:["punishment", "hit and run", "strike", "bang and jump", "aggressiveness"],
+        answer: 2
+    },
+    {
+        q:"Laws have their roots in the ............... of justice.",
+        options:["principal", "prejudice", "preface", "principle", "proportion"],
+        answer: 3
+    },
+    {
+        q:"Don't pay attention to him. I mean, just ............... him.",
+        options:["run away from", "misunderstand", "hit and hurt", "bless and honour", "ignore"],
+        answer: 4
+    },
+    {
+        q:"He knows very well what he is doing. I mean, he is doing it ...............    .",
+        options:["on purpose", "out of good-will", "hesitatingly", "unknowingly", "in ignorance"],
+        answer: 0
+    },
+    {
+     q:"I had to wait in a ............... for over two hours in order to get these tickets.",
+        options:["men's room", "queue", "loo", "wardrobe", "tail"],
+        answer: 1   
+    },
+    {
+        q:"You mustn't apply the ............... too suddenly.",
+        options:["breaks", "tyres", "brakes", "signals", "stop lights"],
+        answer: 2
+    },
+    {
+        q:"It's very ............... work. I get awfully tired by the time I finish for the day.",
+        options:["tedious", "important", "steady", "demanding", "rewarding"],
+        answer: 3
+    },
+    {
+        q:"It's a very busy restaurant on Saturdays; so, don't forget to ............... a table in advance.",
+        options:["bet over", "blend with", "bind", "notebook", "book"],
+        answer: 4
+    }
+]
